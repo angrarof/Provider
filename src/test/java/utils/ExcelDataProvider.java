3c466b72f68374eac229fbf1250611d73c22272c
angrarof <angel.aguilar0@hotmail.com>
2020-04-23 20:46:58 -0500

    Updated data provider excel

--- src/test/java/utils/ExcelDataProvider.java
+++ src/test/java/utils/ExcelDataProvider.java
@@ -4,20 +4,33 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class ExcelDataProvider {
 
     @Test(dataProvider = "getData")
-    public void testingDataProvider(String username, String password){
+    public void printUsers(String username, String password){
         System.out.println(username+"|"+password);
     }
 
+    @Test(dataProvider = "getData")
+    public void printAnother(String d1, String d2, String d3){
+        System.out.println(d1+" "+d2+" "+d3);
+    }
+
     @DataProvider
-    public Object[][] getData() throws IOException {
-        Object data[][] = testData("users",System.getProperty("user.dir")+"/spreads/data.xlsx");
+    public Object[][] getData(Method m) throws IOException {
+        Object[][] data;
+        String sheet = null;
+        if (m.getName().equalsIgnoreCase("printAnother")){
+            sheet = "another";
+        }else if(m.getName().equalsIgnoreCase("printUsers")){
+            sheet = "users";
+        }
+        data=createData(sheet,System.getProperty("user.dir")+"/spreads/data.xlsx");
         return data;
     }
-    public Object[][] testData(String sheetName, String excelPath) throws IOException {
+    public Object[][] createData(String sheetName, String excelPath) throws IOException {
         ExcelUtils excelUtils = new ExcelUtils(sheetName,excelPath);
         int rowCount = excelUtils.getRowCount();
         int columnCount = excelUtils.getColumnCount();
